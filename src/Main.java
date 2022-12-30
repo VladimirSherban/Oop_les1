7b765174d019308f6602759660e499e3be2a8495
VladimirSherban <Vovan_Sh_94@mail.ru>
2022-12-30 05:28:28 +0300

    Homework2 done!

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,36 @@
+import transport.Car;
+
+
 public class Main {
     public static void main(String[] args) {
 
+
         Car ladaGranta = new Car("Lada", "Granta", 1.7,
-                "Желтый", 2015, "Россия");
+                "Желтый", 2015, "Россия", "Седан", 5,
+                "1аам23_01", "автоматическая коробка передач",
+                new Car.Key(false, false));
 
         Car audiA8 = new Car("Audi", "A8", 3.0,
-                "черный", 2020, "Германия");
+                "черный", 2020, "Германия", "Хетчбек", 6,
+                "9арн12_93", "автоматическая коробка передач",
+                new Car.Key(true, true));
 
         Car bMWZ8 = new Car("BMW", "Z8", 3.0,
-                "черный", 2021, "Германия");
+                "черный", 2021, "Германия", "Седан", 6,
+                "0хам01_103", "вариативная коробка передач",
+                new Car.Key(true, false));
 
         Car kiaSportage = new Car("Kia", "Sportage 4", 2.4,
-                "красный", 2018, "Южная Корея");
+                "красный", 2018, "Южная Корея", "Седан", 5,
+                "5ррн23_93", "механическая коробка передач",
+                new Car.Key(false, true));
 
         Car hyundaiAvante = new Car(null, "", -1.3,
-                "  ", -23, "   ");
+                "  ", -23, "   ", "", -6,
+                "     ", "", new Car.Key(false,
+                false));
+
+
 
         System.out.println(ladaGranta);
         System.out.println(audiA8);
@@ -25,3 +41,4 @@ public class Main {
 
     }
 }
+
